c6fb4a47b140229fd81708ffab84aee3e80978ce
natan-poroshin <natan.poroshin@gmail.com>
2022-08-17 06:20:55 +0300

    feat: implemented guessing game.

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,25 @@
 class GuessingGame {
     constructor() {}
 
-    setRange(min, max) {
+    #number = 0;
+    #left = 0;
+    #right = 0;
 
+    setRange(min, max) {
+        this.#left = min;
+        this.#right = max;
     }
 
     guess() {
-
+        return this.#number = this.#left + Math.ceil((this.#right - this.#left) / 2);
     }
 
     lower() {
-
+        this.#right = this.#number;
     }
 
     greater() {
-
+        this.#left = this.#number;
     }
 }
 
